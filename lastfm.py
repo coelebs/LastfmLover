3158cf4fdc29fa58d692b5c59d30cad4c08515c9
Vincent Kriek <vincent@vincentkriek.nl>
2011-01-06 11:55:23 +0100

    Added zenity

--- lastfm.py
+++ lastfm.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python
 import pylast
 
+from subprocess import Popen
+
 API_KEY = "40887e583290b0d8932e3c872ac7aae5"
 API_SECRET = "6605f506e40adf0ad7eb29da94fafa42"
 
 username = "Vintendo"
 password_hash = "c9c952008479e67d04709809ac61fc57"
 
-network = pylast.LastFMNetwork(api_key = API_KEY, api_secret = API_SECRET,
-                        username = username, password_hash = password_hash)
+def getTrack():
+    network = pylast.LastFMNetwork(api_key = API_KEY, api_secret = API_SECRET,
+                            username = username, password_hash = password_hash)
 
-user = network.get_authenticated_user()
-track = user.get_now_playing()
+    user = network.get_authenticated_user()
+    track = user.get_now_playing()
 
-if not track:
-    track = user.get_recent_tracks(limit = 1)[0]
+    if not track:
+        track = user.get_recent_tracks(limit = 1)[0]
 
-answer = raw_input("Do you want to love %s? (Y/n) " % track)
+    return track;
 
-if answer is 'y' or not answer:
-    track.love()
+
+if __name__ == '__main__':
+    track = getTrack()
+    
+    question = "Do you want to love %s?" % track
+
+    proc = Popen("zenity --question --text='%s'" % question, shell=True)
+    proc.communicate()
+    if proc.returncode:
+        track.love()
